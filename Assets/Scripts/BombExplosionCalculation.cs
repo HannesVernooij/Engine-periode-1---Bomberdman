6ea6d7c7428980b6ea5b7020b6dbcb929d3e9a2d
sakirma <huseyin.32@live.nl>
2015-09-29 16:30:33 +0200

    Script voor Bomb Explosion Calculation
    dsfsdfohfsduhsdfouf FAGGOTS

---
+++ Assets/Scripts/BombExplosionCalculation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombExplosionCalculation : MonoBehaviour
+{
+    private MapGenerator _MapGenerator;
+    private int[,] BombPosition;
+
+    void Start()
+    {
+        _MapGenerator = GameObject.FindObjectOfType<MapGenerator>();
+    }
+    public void GetBombInformation(int x, int y)
+    {
+        BombPosition = new int[x, y];
+    }
+
+    
+}
