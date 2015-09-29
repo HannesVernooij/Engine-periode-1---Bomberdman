56920eb84d28546f714ff9b898643397b73c7b5d
N-Meijer <n-meijer@live.nl>
2015-09-29 16:07:55 +0200

    Very naice mapgenerator
    Make all dem blocks and floor

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,15 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject wall;
+    public GameObject floorPrefab;
+    GameObject floor;
+    //Width & Height should always be uneven
+    public int mapWidth;
+    public int mapHeight;
+    GameObject[,] mapArray;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Z = Width X = Height because fuck logic
+
+    void Start()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        mapWidth = Mathf.Abs(mapWidth);
+        mapHeight = Mathf.Abs(mapHeight);
+
+        mapArray = new GameObject[mapHeight, mapWidth];
+
+        floor = Instantiate(floorPrefab, new Vector3(mapHeight / 2, 0, mapWidth / 2), Quaternion.identity) as GameObject;
+        floor.transform.localScale = new Vector3(floor.transform.localScale.x * mapHeight, 1, floor.transform.localScale.z * mapWidth);
+
+        for (int i = 0; i < mapHeight; i++)
+        {
+            for (int j = 0; j < mapWidth; j++)
+            {
+                //Walls for around map
+                if (i == 0 || i == mapHeight-1)
+                {
+                    mapArray[i,j] = Instantiate(wall, new Vector3(i, 1, j),Quaternion.identity) as GameObject;
+                }
+
+                else if(j == 0 || j == mapWidth-1)
+                {
+                    mapArray[i, j] = Instantiate(wall, new Vector3(i, 1, j), Quaternion.identity) as GameObject;
+                }
+
+                //Block placement
+                else if(i != 1 && i != mapHeight-2 && j != 1 && j != mapWidth-2 && !IsOdd(j) && !IsOdd(i))
+                {
+                    mapArray[i, j] = Instantiate(wall, new Vector3(i, 1, j), Quaternion.identity) as GameObject;
+                }
+            }
+        }
+    }
+
+    static bool IsOdd(int value)
+    {
+        return value % 2 != 0;
+    }
+
+    public bool GetTile(int y, int x)
+    {
+        if (mapArray[y,x] == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
